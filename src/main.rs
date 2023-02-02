d8c8624176f682e4874ed10df4e602a33f07aa13
sny430 <shinya430.g@gmail.com>
2023-02-02 15:15:12 +0900

    :construction: combine linebot and openai

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use openai_api_rs::v1::completion::{self, CompletionRequest};
-use openai_api_rs::v1::api::Client;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+extern crate line_bot_sdk_rust as line;
+
 use std::env;
 use dotenv::dotenv;
+use rocket::http::Status;
 use futures::executor::block_on;
 
+use line::bot::LineBot;
+use line::events::messages::MessageType as EventMessageType;
+use line::events::{EventType, Events};
+use line::messages::{SendMessageType, TextMessage};
+use line::support::rocket_support::{Body, Signature};
+
+use openai_api_rs::v1::completion::{self, CompletionRequest};
+use openai_api_rs::v1::api::Client;
+
 async fn openai(question: String) -> Result<String, Box<dyn std::error::Error>> {
-    dotenv().ok();
     let client = Client::new(env::var("OPENAI_API_KEY").unwrap().to_string());
     let req = CompletionRequest {
         model: completion::GPT3_TEXT_DAVINCI_003.to_string(),
@@ -31,10 +44,51 @@ async fn openai(question: String) -> Result<String, Box<dyn std::error::Error>> 
     Ok(answer)
 }
 
+#[post("/callback", data = "<body>")]
+fn callback(signature: Signature, body: Body) -> Status {
+    // Get channel secret and access token by environment variable
+    let channel_secret: &str =
+        &env::var("LINE_CHANNEL_SECRET").expect("Failed getting LINE_CHANNEL_SECRET");
+    let access_token: &str =
+        &env::var("LINE_CHANNEL_ACCESS_TOKEN").expect("Failed getting LINE_CHANNEL_ACCESS_TOKEN");
+
+    // LineBot
+    let bot = LineBot::new(channel_secret, access_token);
+
+    // Request body parse
+    let result: Result<Events, &'static str> =
+        bot.parse_event_request(&signature.key, &body.string);
+
+    // Success parsing
+    if let Ok(res) = result {
+        for event in res.events {
+            // MessageEvent only
+            if let EventType::MessageEvent(message_event) = event.r#type {
+                // TextMessageEvent only
+                if let EventMessageType::TextMessage(text_message) = message_event.message.r#type {
+                    // Get answer by OpenAI
+                    let answer = block_on(openai(text_message.text)).ok().unwrap();
+                    // Create TextMessage
+                    let message = SendMessageType::TextMessage(TextMessage {
+                        text: answer,
+                        emojis: None,
+                    });
+                    // Reply message with reply_token
+                    let _res = bot.reply_message(&message_event.reply_token, vec![message]);
+                }
+            }
+        }
+        return Status::new(200, "OK");
+    }
+    // Failed parsing
+    else if let Err(msg) = result {
+        return Status::new(500, msg);
+    }
+    Status::new(500, "Internal Server Error")
+}
+
 #[tokio::main]
 async fn main() {
-    let mut question = String::new();
-    std::io::stdin().read_line(&mut question);
-    let answer = block_on(openai(question));
-    println!("{:?}", answer);
+    dotenv().ok();
+    rocket::ignite().mount("/", routes![callback]).launch();
 }
